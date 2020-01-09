6d190f3839c2c88b0edd0ca3e9e4858ad82669a7
imdevil <516959708@qq.com>
2020-01-09 21:25:09 +0800

    Initial commit

---
+++ app/src/main/java/com/imdevil/onemusic/MainActivity.java
@@ -0,0 +1,13 @@
+package com.imdevil.onemusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
