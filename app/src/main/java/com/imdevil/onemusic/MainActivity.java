30fc8ceea920b25cc07ff7e516a283351b7836b3
imdevil <516959708@qq.com>
2020-01-10 00:52:55 +0800

    添加DrawerLayout和TabLayout

--- app/src/main/java/com/imdevil/onemusic/MainActivity.java
+++ app/src/main/java/com/imdevil/onemusic/MainActivity.java
@@ -1,13 +1,19 @@
 package com.imdevil.onemusic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
